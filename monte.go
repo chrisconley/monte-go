1a26becad2422b982a875ad326b2c1a62e5519b4
Chris Conley <chris.m.conley@gmail.com>
2013-08-05 17:11:21 -0400

    alright, stdin and stdout working along with arg parsing

--- monte.go
+++ monte.go
@@ -1,1 +1,33 @@
-package monte
+// Usage:
+// echo -e "hi\nbye" | (go build monte.go && ./monte --flagname=12) > ./test.txt
+package main
+
+import (
+  "flag"
+  "fmt"
+  "os"
+  "bufio"
+)
+
+func main() {
+  var ip = flag.Int("flagname", 1234, "help message for flagname")
+  flag.Parse()
+  //fmt.Printf("%d\n", *ip)
+  reader := bufio.NewReader(os.Stdin)
+  out := bufio.NewWriter(os.Stdout)
+  // This should be fleshed out a bit with: http://crypto.stanford.edu/~blynn/c2go/ch02.html
+  for {
+    line, err := reader.ReadString('\n')
+
+    if err != nil {
+      // You may check here if err == io.EOF
+      break
+    }
+    out.WriteString(fmt.Sprintf("%d, %s", *ip, line))
+    out.Flush()
+
+    //fmt.Printf("%s\n", line)
+  }
+}
+
+
